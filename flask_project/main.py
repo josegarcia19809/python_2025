8f18b831cdb2272043cb2f9126ab15af92912f5a
Jose Garcia <josegarcia19809@gmail.com>
2025-04-04 10:53:11 -0600

    Flask: agregando más rutas

--- flask_project/main.py
+++ flask_project/main.py
@@ -11,6 +11,21 @@ def index():
 def informacion():
     return "<h1>Página de información</h1>"
 
+@app.route("/contacto") # http://127.0.0.1:5000/contacto
+def contacto():
+    return "<h1>Mi nombre es José Luis García</h1>"
+
+@app.route("/lenguajes-de-programacion") # http://127.0.0.1:5000/lenguajes-de-programacion
+def lenguajes():
+    return """
+    <h1>Lenguajes de programación</h1>
+    <p>Python</p>
+    <p>Java</p>
+    <p>JavaScript</p>
+    <p>C++</p>
+    <p>C#</p>
+    """
+
 
 
 if __name__ == '__main__':
